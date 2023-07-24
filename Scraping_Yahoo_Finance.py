e44e10bcc3728cabeed22391f73680fe977afef2
Talha <mtalhaasifshazad@gmail.com>
2023-07-25 04:42:10 +0500

    Subject: Database Integeration
    In this phase, I integerate MySQL database with python to store data.
    During that process first it's take information for bulding database
    connection after connecting it's check wheter the desire table is present or
    not in a database if not it will create it first. After checking table
    it will insert scrap data into MySQL database.

--- Scraping_Yahoo_Finance.py
+++ Scraping_Yahoo_Finance.py
@@ -2,6 +2,71 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
 
+import mysql.connector
+
+def connection_database(host,user,password,database):
+    try:
+        # logging.info('Building Database Connection')
+        conn = mysql.connector.connect(
+                host= host,
+                user= user,     
+                password= password, 
+                database= database 
+            )
+    except Exception as e:
+        print(e)
+    else:
+        return conn
+
+def Table_creation(connection,database):
+    conn = connection
+    cursor = conn.cursor()
+    table_name = "Finance_Data"
+
+# MySQL query that give result if table exist
+    table_exists_query = """
+    SELECT COUNT(*)
+    FROM information_schema.tables
+    WHERE table_schema = '{database}'
+        AND table_name = '{table}'
+    """.format(database= database, table=table_name)
+
+    cursor.execute(table_exists_query)
+    table_exists = cursor.fetchone()[0]
+
+# Checking Table exist or not
+    if table_exists:
+        # Table exists, so delete it
+        delete_table_query = "DROP TABLE {table}".format(table=table_name)
+        cursor.execute(delete_table_query)
+        # logging.info("Table deleted successfully.")
+    else:
+        # logging.info("Table does not exist.")
+        print("Table does not exist.")
+
+# MySQL query for creating table
+    create_table_query = """
+    CREATE TABLE {table} (
+        Id INT AUTO_INCREMENT PRIMARY KEY,
+        Symbol VARCHAR(50),
+        Name VARCHAR(200),
+        URL VARCHAR(500),
+        Last_Price FLOAT,
+        Change_ VARCHAR(50),
+        Percentage_Change VARCHAR(50)
+    );
+    """.format(table=table_name)
+
+    # Execute the table creation query
+    try:
+        cursor.execute(create_table_query)
+    except Exception as e:
+        # logging.error(e)
+        print(e)
+    else:
+        # logging.info("Table created successfully.")
+        print('Success')
+
 def webdriver_connection():
     '''
     Keep your chromedriver file path in project folder and can manully change executable path,
@@ -26,34 +91,43 @@ def main_site_opening(driver):
     except Exception as e:
         print(e)
 
-def Scraping_Data():
+def Scraping_Data(connection):
     driver = webdriver_connection()
     main_site_opening(driver=driver)
 
     xpaths = '//*[@id="lookup-page"]/section/div/div/div/table/tbody/tr/td/a'
-    Synbol = driver.find_elements(by='xpath',value= xpaths)
+    Symbols = driver.find_elements(by='xpath',value= xpaths)
 
     name_xpath = '//*[@id="lookup-page"]/section/div/div/div/table/tbody/tr/td[2]'
-    name = driver.find_elements(by='xpath',value=name_xpath)
+    names = driver.find_elements(by='xpath',value=name_xpath)
 
     L_Price_xpath = '//*[@id="lookup-page"]/section/div/div/div/table/tbody/tr/td[3]'
-    Last_Price = driver.find_elements(by='xpath',value=L_Price_xpath)
+    Last_Prices = driver.find_elements(by='xpath',value=L_Price_xpath)
 
     change_xpath = '//*[@id="lookup-page"]/section/div/div/div/table/tbody/tr/td[4]/span'
-    change = driver.find_elements(by='xpath',value=change_xpath)
+    changes = driver.find_elements(by='xpath',value=change_xpath)
 
     per_change_xpath = '//*[@id="lookup-page"]/section/div/div/div/table/tbody/tr/td[5]/span'
-    per_change = driver.find_elements(by='xpath',value=per_change_xpath)
+    per_changes = driver.find_elements(by='xpath',value=per_change_xpath)
 
-    for s,n,l,c,p in zip(Synbol,name,Last_Price,change,per_change):
+    for symbol,name,last_price,change,per_change in zip(Symbols,names,Last_Prices,changes,per_changes):
 
-        print(s.text)
-        print(s.get_attribute('href'))
-        print(n.text)
-        print(l.text)
-        print(c.text)
-        print(p.text)
+        conn = connection
+        cursor = conn.cursor()
+        insert_query = f"INSERT INTO Finance_Data (Symbol, Name, URL, Last_Price, Change_, Percentage_Change) VALUES (%s,%s,%s,%s,%s,%s)"
+        data = (symbol.text, name.text, symbol.get_attribute('href'), float(last_price.text), change.text,per_change.text)
+        cursor.execute(insert_query,data)
+        conn.commit()
     driver.close()
 
 if __name__ == '__main__':
-    Scraping_Data()
+    
+    print("\nGive following information to build MySQL connection.")
+    Host = input("Enter your host.\n")
+    User = input("Enter your MySQL username.\n")
+    Password = input("Enter your MySQL password.\n")
+    Database =  input("Enter your MySQL database name.\n")
+
+    conn = connection_database(Host,User,Password,Database)
+    Table_creation(conn,Database)
+    Scraping_Data(conn)
