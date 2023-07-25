20b31f17842926b3bbb60e1cd4e3369d241933b8
Talha <mtalhaasifshazad@gmail.com>
2023-07-26 03:19:42 +0500

    Subject: Implement others CRUD operations
    In this phase, I implemented other CRUD operations, like post for adding
    record, put for update record, delete for deleting the record.

--- Finance_API.py
+++ Finance_API.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
 
@@ -63,5 +63,60 @@ def get_all_data():
         Finance_data.append(data)
     return jsonify(Finance_data)
 
+@app.route('/get-specific-record/<int:record_id>/', methods=['GET'])
+def get_specific_record(record_id):
+    record = finance_data.query.get(record_id)
+    if not record:
+        return jsonify({'message': 'Record not found'}), 404
+
+    data = {
+        'Id': record.Id,
+        'Symbol': record.Symbol,
+        'Name': record.Name,
+        'URL': record.URL,
+        'Last_Price': record.Last_Price,
+        'Change_': record.Change_,
+        'Percentage_Change': record.Percentage_Change
+    }
+    return jsonify(data)
+
+@app.route('/add-data/', methods=['POST'])
+def add_data():
+    data = finance_data(
+            Symbol = request.json['symbol'],
+            Name= request.json['name'],
+            URL= request.json['url'],
+            Last_Price= request.json['last_price'],
+            Change_= request.json['change_'],
+            Percentage_Change= request.json['percentage_change'])
+    db.session.add(data)
+    db.session.commit()
+    return {'id':data.Id}
+
+@app.route('/update-record/<int:record_id>/', methods=['PUT'])
+def update_record(record_id):
+    record = db.session.query(finance_data).filter_by(Id=record_id).first()
+    if not record:
+        return jsonify({'message': 'Record not found'}), 404
+
+    record.Symbol = request.json['symbol']
+    record.Name = request.json['name']
+    record.URL = request.json['url']
+    record.Last_Price = request.json['last_price']
+    record.Change_ = request.json['change_']
+    record.Percentage_Change = request.json['percentage_change']
+
+    db.session.commit()
+    return jsonify({'message': 'Record updated successfully'})
+
+@app.route('/delete-record/<int:record_id>/', methods=['DELETE'])
+def delete_record(record_id):
+    record = finance_data.query.get(record_id)
+    if not record:
+        return jsonify({'message': 'Record not found'}), 404
+
+    db.session.delete(record)
+    db.session.commit()
+    return jsonify({'message': 'Record deleted successfully'})
 if __name__ == "__main__":
     app.run()
