ae212bfb8d6cd44f27fe2813215a30d209236eb2
Talha <mtalhaasifshazad@gmail.com>
2023-07-26 11:40:31 +0500

    added requirement.txt file you can install all packages necessary for this project using this file

--- Finance_API.py
+++ Finance_API.py
@@ -3,7 +3,6 @@ from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
 from Scraping_Yahoo_Finance import Scraping_Data
-import datetime
 
 logging.basicConfig(filename='app.log', filemode='w',level=logging.INFO, format= '%(asctime)s - %(levelname)s - %(message)s - %(lineno)d')
 
